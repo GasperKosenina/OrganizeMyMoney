460af02616428f1ced233a7a46639c574da3a3ff
GasperKosenina <gasper.kosenina@student.um.si>
2022-12-19 16:35:01 +0100

    baza #OMM-24 DONE Time 1h

--- src/main/java/com/organizemymoney/ris/Models/Bancni_racun.java
+++ src/main/java/com/organizemymoney/ris/Models/Bancni_racun.java
@@ -1,8 +1,29 @@
 package com.organizemymoney.ris.Models;
+import jakarta.persistence.*;
 
+@Entity
 public class Bancni_racun {
 
-	private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", nullable = false)
+	private int id;
 	private int stanje;
 
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setStanje(int stanje) {
+		this.stanje = stanje;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getStanje() {
+		return stanje;
+	}
 }
